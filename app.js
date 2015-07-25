08367f1e812e9759103e5b7e2a73ecc723b641af
Marcelo Tello <marcelotello@gmail.com>
2015-07-25 15:32:39 +0200

    Modulo 9: Bug login-redir problem

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
+	if (!req.session.redir) {                                // si no existe lo inicializa
+             req.session.redir = '/';
+        }
   if (!req.path.match(/\/login|\/logout/)){
 	  console.log("REDIR="+req.path);
 	  req.session.redir=req.path;
